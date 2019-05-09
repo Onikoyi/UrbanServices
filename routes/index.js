0b0ef3543762ee31ee4b56e67bb0e623307bf642
Onikoyi Tajudeen Tunde <onikstib2007@yahoo.com>
2019-05-09 08:47:32 +0100

    Admin Page Added

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'UrbanServices' });
-  
+});
+  router.get('/adminPage', function(req, res, next) {
+    res.render('adminPage', { title: 'Admin' });
 });
 
 module.exports = router;
