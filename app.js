c37676efc652d6ee8b624d093cf19a6103d4b960
Onikoyi Tajudeen Tunde <onikstib2007@yahoo.com>
2019-05-27 07:40:54 +0100

    ContactUs modified

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+// var bodyparser = require('body-parser');
 var logger = require('morgan');
+// var favicon = require('save-favicon');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
